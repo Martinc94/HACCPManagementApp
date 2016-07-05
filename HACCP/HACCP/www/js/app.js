c5a98958a8d58a2056f862d13c804b3aa59e7d67
UnderDev <scottcoyne@hotmail.com>
2016-07-05 15:10:15 +0100

    Basic Layout/Navigation

--- HACCP/HACCP/www/js/app.js
+++ HACCP/HACCP/www/js/app.js
@@ -5,7 +5,9 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','tabSlideBox'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -55,12 +57,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.home', {
+      url: '/home',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+          'tab-home': {
+              templateUrl: 'templates/tab-home.html',
+              controller: 'IntroCtrl'
         }
       }
     })
@@ -84,8 +86,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
 
+
   // if none of the above states are matched, use this as the fallback
   //$urlRouterProvider.otherwise('/tab/dash');
   $urlRouterProvider.otherwise('/login');
 
 });
+
+
