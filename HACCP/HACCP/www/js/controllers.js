1e568569ef0ea4f663b0f5940c8777591ac83d0a
Martinc94 <martinc94@live.co.uk>
2017-02-28 22:13:39 +0000

    Added removed method

--- HACCP/HACCP/www/js/controllers.js
+++ HACCP/HACCP/www/js/controllers.js
@@ -293,6 +293,28 @@ angular.module('starter.controllers', ['ionic.wheel'])
 
     });
 
+    $scope.takePicture = function () {
+      var options = {
+        quality: 75,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        popoverOptions: CameraPopoverOptions,
+        cameraDirection: 1,
+        saveToPhotoAlbum: false
+      };
+
+      $cordovaCamera.getPicture(options).then(function (imageData) {
+        $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        $scope.toggle = true;
+      }, function (err) {
+        // An error occured. Show a message to the user
+      });
+    }
+
     $scope.getLocation = function () {
       LocationService.getLocation().then(function (loc) {
         $scope.deliveryForm.lat = loc.lat;
